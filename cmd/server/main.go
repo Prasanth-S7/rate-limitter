6a5d2a33264b808efa4336cdaa6d180fbcc6031e
Sampath <prasanthsampath2005@gmail.com>
2026-02-05 10:51:38 +0530

    refactor: organized the folder structure

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,73 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"sync"
-	"time"
+
+	"github.com/Prasanth-S7/rate-limitter/internal/ratelimiter"
+	"github.com/Prasanth-S7/rate-limitter/pkg/middleware"
 )
-
-type tokenBucket struct {
-	capacity   int
-	tokens     int
-	fillRate   time.Duration
-	lastRefill time.Time
-	mu         sync.Mutex
-}
-
-func NewTokenBucket(capacity int, fillRate float64) *tokenBucket {
-	return &tokenBucket{
-		capacity:   capacity,
-		tokens:     capacity,
-		fillRate:   time.Duration(float64(time.Second) / fillRate),
-		lastRefill: time.Now(),
-	}
-}
-
-// checking if a token can be consumed
-func (tb *tokenBucket) Allow() bool {
-	tb.mu.Lock()
-	defer tb.mu.Unlock()
-
-	tb.refill()
-
-	if tb.tokens > 0 {
-		tb.tokens--
-		return true
-	}
-	return false
-}
-
-// refilling token logic
-func (tb *tokenBucket) refill() {
-	now := time.Now()
-	elapsed := now.Sub(tb.lastRefill)
-
-	tokensToAdd := int(elapsed / tb.fillRate)
-
-	if tokensToAdd > 0 {
-		tb.tokens += tokensToAdd
-		if tb.tokens > tb.capacity {
-			tb.tokens = tb.capacity
-		}
-	}
-
-	tb.lastRefill = tb.lastRefill.Add(time.Duration(tokensToAdd) * tb.fillRate)
-}
-
-func (tb *tokenBucket) middleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !tb.Allow() {
-			http.Error(w, "Too Many Requests", http.StatusTooManyRequests)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
 
 func getHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, Gophers! You requested: %s\n", r.URL.Path)
 }
 
 func main() {
-	tb := NewTokenBucket(10, 2)
-	http.Handle("/", tb.middleware(http.HandlerFunc(getHandler)))
+	tb := ratelimiter.NewTokenBucket(10, 2)
+	rl := middleware.New(tb)
+
+	http.Handle("/", rl.Middleware(http.HandlerFunc(getHandler)))
+	http.ListenAndServe(":8000", nil)
 }
